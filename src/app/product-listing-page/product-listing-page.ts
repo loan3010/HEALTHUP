a9f84e0560328db0a7852eff76e46a44f78ab781
PHUONG LOAN <@loan3010>
2026-02-15 19:44:52 +0700

    Add Angular project structure

---
+++ src/app/product-listing-page/product-listing-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-listing-page',
+  imports: [],
+  templateUrl: './product-listing-page.html',
+  styleUrl: './product-listing-page.css',
+})
+export class ProductListingPage {
+
+}
